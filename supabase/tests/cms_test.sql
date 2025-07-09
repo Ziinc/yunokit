733c266dc835d06aca8c97001897da7d44239039
TzeYiing <ty@tzeyiing.com>
2025-07-09 09:56:16 +0800

    chore: update pg_tap tests

--- supabase/tests/cms_test.sql
+++ supabase/tests/cms_test.sql
@@ -1,61 +1,94 @@
 BEGIN;
-SELECT plan(7);
+SELECT plan(12);
 
 -- CONTENT ITEMS
 SELECT columns_are(
     'yunocontent',
     'content_items',
-    ARRAY['id', 'content_type_id', 'data', 'inserted_at', 'updated_at', 'user_id' ]
+    ARRAY['id', 'uid', 'title', 'data', 'schema_id', 'created_at', 'updated_at', 'published_at', 'deleted_at']
 );
 
-
-SELECT triggers_are(
-    'yunocontent',
-    'content_items',
-    ARRAY[ 'tr_content_items_bu']
-);
-
-
--- CONTENT TYPES
-
+-- SCHEMAS (formerly content_types)
 SELECT columns_are(
     'yunocontent',
-    'content_types',
-    ARRAY['id', 'fields', 'name', 'type', 'inserted_at', 'updated_at']
+    'schemas',
+    ARRAY['id', 'name', 'description', 'type', 'fields', 'created_at', 'updated_at', 'archived_at', 'deleted_at']
 );
 
-
-
-SELECT triggers_are(
+-- AUTHORS
+SELECT columns_are(
     'yunocontent',
-    'content_types',
-    ARRAY[ 'tr_content_types_bu']
+    'authors',
+    ARRAY['id', 'sc_user_id', 'user_id', 'first_name', 'last_name', 'pseudonym', 'title', 'description', 'metadata']
 );
 
+-- CONTENT AUTHORS
+SELECT columns_are(
+    'yunocontent',
+    'content_authors',
+    ARRAY['id', 'content_id', 'author_id']
+);
 
+-- Check schema_type enum values
+SELECT enum_has_labels(
+    'yunocontent',
+    'schema_type',
+    ARRAY['single', 'collection']
+);
 
--- restricted column values
-prepare insert_type as 
-    INSERT INTO yunocontent.content_types (type, name, fields) 
-    VALUES ('any', 'posts', '[]');
+-- Test foreign key relationships
+SELECT has_fk(
+    'yunocontent',
+    'content_items',
+    'schema_id',
+    'yunocontent',
+    'schemas',
+    'id'
+);
 
-select throws_ok('insert_type');
+SELECT has_fk(
+    'yunocontent',
+    'content_authors',
+    'author_id',
+    'yunocontent',
+    'authors',
+    'id'
+);
 
+-- Test schema type constraints
+prepare insert_invalid_type as 
+    INSERT INTO yunocontent.schemas (type, name, fields) 
+    VALUES ('invalid', 'posts', '[]');
 
--- restricted column values
-prepare insert_name as 
-    INSERT INTO yunocontent.content_types (type, name, fields) 
-    VALUES ('single', 'p', '[]');
+select throws_ok('insert_invalid_type');
 
-select throws_ok('insert_name');
+-- Test valid schema type
+prepare insert_valid_type as 
+    INSERT INTO yunocontent.schemas (type, name, fields) 
+    VALUES ('single', 'posts', '[]');
 
+select lives_ok('insert_valid_type');
 
-prepare insert_fields as 
-    INSERT INTO yunocontent.content_types (type, name, fields) 
-    VALUES ('single', 'posts', '{}');
+-- Test content_items uid uniqueness
+prepare insert_duplicate_uid as
+    INSERT INTO yunocontent.content_items (uid, title) 
+    VALUES ('test-uid', 'Test 1'), ('test-uid', 'Test 2');
 
-select throws_ok('insert_fields');
+select throws_ok('insert_duplicate_uid');
 
+-- Test valid content item creation
+prepare insert_valid_content as
+    INSERT INTO yunocontent.content_items (uid, title, data) 
+    VALUES ('valid-uid', 'Valid Content', '{"test": "data"}');
+
+select lives_ok('insert_valid_content');
+
+-- Test author creation
+prepare insert_author as
+    INSERT INTO yunocontent.authors (first_name, last_name, pseudonym) 
+    VALUES ('John', 'Doe', 'johndoe');
+
+select lives_ok('insert_author');
 
 -- Finish the tests and clean up.
 SELECT * FROM finish();
