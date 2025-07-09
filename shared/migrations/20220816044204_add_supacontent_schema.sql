94aeddec9797c7a0ef96d459456830c4f8682d2e
TzeYiing <ty@tzeyiing.com>
2025-07-09 09:50:48 +0800

    chore: remove unused migration files

--- shared/migrations/20220816044204_add_supacontent_schema.sql
+++
@@ -1,5 +0,0 @@
-create schema yunocontent;
-
-grant usage on schema yunocontent to postgres, anon, authenticated, service_role, supabase_admin;
-grant all privileges on all tables in schema yunocontent to postgres, anon, authenticated, service_role, supabase_admin;
-alter default privileges in schema yunocontent grant all on tables to postgres, anon, authenticated, service_role, supabase_admin;
