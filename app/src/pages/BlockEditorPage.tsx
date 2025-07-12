16b4587b76bec369bdea388b382a59fbd313a2d4
Ziinc <Ziinc@users.noreply.github.com>
2025-07-12 09:09:19 +0800

    chore: Remove legacy editor routes and components (#2)

--- app/src/pages/BlockEditorPage.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import { BlockEditor } from "@/components/Editors/BlockEditor";
-
-const BlockEditorPage: React.FC = () => {
-  return <BlockEditor />;
-};
-
-export default BlockEditorPage;
--- app/src/pages/JsonEditorPage.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import { JSONEditor } from "@/components/Editors/JSONEditor";
-
-const JsonEditorPage: React.FC = () => {
-  return <JSONEditor />;
-};
-
-export default JsonEditorPage;
